3e9fd153e3be00519bd4989b6149bf00c0f6fe44
Resky Ramadhandi S <reskyramadhandi@gmail.com>
2022-02-04 11:26:58 +0700

    configure database settings

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'DjangoAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoAPI',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'foxtrot-server',
+        'PORT': '5432'
     }
 }
 
