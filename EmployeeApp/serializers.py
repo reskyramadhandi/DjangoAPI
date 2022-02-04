d17259c666dbe1ab1ca6c583be875fad77bc1d2e
Resky Ramadhandi S <reskyramadhandi@gmail.com>
2022-02-04 20:38:14 +0700

    model serializers

---
+++ EmployeeApp/serializers.py
@@ -0,0 +1,17 @@
+import imp
+from matplotlib.pyplot import cla
+from rest_framework import serializers
+from EmployeeApp.models import Departments, Employees
+
+
+class DepartmentSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Departments
+        fields = ('DepartmentId', 'DepartmentName')
+
+
+class EmployeeSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = ('EmployeeId', 'EmployeeName', 'DateOfJoining',
+                  'PhotoFileName', 'DepartmentId')
