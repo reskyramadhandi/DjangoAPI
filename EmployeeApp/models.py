4c94cc2bda3627d230ead6cf417b97c0ff558de4
Resky Ramadhandi S <reskyramadhandi@gmail.com>
2022-02-07 10:57:42 +0700

    fixing employee model

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -11,6 +11,6 @@ class Departments(models.Model):
 class Employees(models.Model):
     EmployeeId = models.AutoField(primary_key=True)
     EmployeeName = models.CharField(max_length=500)
-    Department = models.CharField(max_length=500)
     DateOfJoining = models.DateField()
     PhotoFileName = models.CharField(max_length=500)
+    DepartmentId = models.IntegerField()
