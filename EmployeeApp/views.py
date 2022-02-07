be3b523c7ba99f1a8720d3ebba500588c141d6cf
Resky Ramadhandi S <reskyramadhandi@gmail.com>
2022-02-07 10:57:58 +0700

    Create employee view

--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -37,3 +37,32 @@ def departmentApi(request, id=0):
         department = Departments.objects.get(DepartmentId=id)
         department.delete()
         return JsonResponse("Deleted Sucessfully", safe=False)
+
+
+@csrf_exempt
+def employeeApi(request, id=0):
+    if request.method == 'GET':
+        employees = Employees.objects.all()
+        employees_serializer = EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+    elif request.method == 'POST':
+        employee_data = JSONParser().parse(request)
+        employees_serializer = EmployeeSerializer(data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Added Sucessfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif request.method == 'PUT':
+        employee_data = JSONParser().parse(request)
+        employee = Employees.objects.get(
+            EmployeeId=employee_data['EmployeeId'])
+        employees_serializer = EmployeeSerializer(
+            employee, data=employee_data)
+        if employees_serializer.is_valid():
+            employees_serializer.save()
+            return JsonResponse("Updated Sucessfully", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    elif request.method == 'DELETE':
+        employee = Employees.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted Sucessfully", safe=False)
